efa17b992fbf1582c90df13a15f4a1c48d117f9d
melissareboucas <melissa.mnrv@gmail.com>
2024-03-22 21:40:15 -0300

    Primeiro commit

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,17 +1,5 @@
 import "phaser";
-import LoaderScene from "./scenes/LoaderScene";
-import MenuScene from "./scenes/MenuScene";
-import TutorialScene from "./scenes/TutorialScene";
-import SelectCharacterScene from "./scenes/SelectCharacterScene";
-import SelectLevelScene from "./scenes/SelectLevelScene";
-import ScoreBoardScene from "./scenes/ScoreBoardScene";
-import CreditsScene from "./scenes/CreditsScene";
-import Level1Scene from "./scenes/Level1Scene";
-import Level2Scene from "./scenes/Level2Scene";
-import Level3Scene from "./scenes/Level3Scene";
-import Level4Scene from "./scenes/Level4Scene";
-import YourScoreScene from "./scenes/YourScoreScene";
-import GameOverScene from "./scenes/GameOverScene";
+import MouseScene from "./scenes/MouseScene";
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
@@ -31,19 +19,7 @@ const config: Phaser.Types.Core.GameConfig = {
     },
   },
   scene: [
-    LoaderScene,
-    MenuScene,
-    TutorialScene,
-    SelectCharacterScene,
-    SelectLevelScene,
-    ScoreBoardScene,
-    CreditsScene,
-    Level1Scene,
-    Level2Scene,
-    Level3Scene,
-    Level4Scene,
-    YourScoreScene,
-    GameOverScene
+    MouseScene
   ],
 };
 
